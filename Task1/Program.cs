dec63196b3fbb67b72a490bbcb6bbe24f2f031bc
Anton <balandzin2602@gmail.com>
2022-09-16 16:13:30 +0300

    Задача 1 Домашняя работа

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"Максимальное число: {num1}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {num2}");
+}
