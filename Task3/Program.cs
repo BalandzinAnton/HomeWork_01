494993d672829f570bb9aea69d96417c41230685
Anton <balandzin2602@gmail.com>
2022-09-16 16:27:39 +0300

    Задача 3 Домашняя работа

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
