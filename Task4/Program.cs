d17dd0cfc01cbe2e620c322353fec7dddbdd3367
Anton <balandzin2602@gmail.com>
2022-09-16 15:58:40 +0300

    Задача 4 Домашняя работа

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num >= 1)
+{
+    if (num%2 == 0)
+    {
+    Console.WriteLine($"{num}");
+    }
+    num--;
+}
